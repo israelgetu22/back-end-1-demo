f8a2fb6423748075ea81a40ed0622aead5eeb408
Isarel Getu <israelgetu101@gmail.com>
2022-02-28 18:33:00 -0600

    Well Done

--- server/server.js
+++ server/server.js
@@ -42,3 +42,11 @@ const SERVER_PORT = 5050;
 app.listen(SERVER_PORT, () => {
   console.log(`Server running on ${SERVER_PORT}`);
 });
+
+app.get("/api/name", (req, res) => {
+  console.log(Hello, how are you?);
+  const numIndex = +req.params.id;
+  res.status(200).send(inventory[numIndex]);
+});
+
+ 
